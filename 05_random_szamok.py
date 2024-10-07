411a7f6704e4dbf360cfc390d56bfc76165f5642
szcsd37 <szucs.dominik@diak.szbi-pg.hu>
2024-10-07 10:47:16 +0200

    Add all files

--- 05_random_szamok.py
+++ 05_random_szamok.py
@@ -1,13 +1,26 @@
+#import random
+
+#random_szam = random.randint(1, 3)
+
+#megadott_szam = int(input("Adj meg egy számot! "))
+
+
+#if random_szam == megadott_szam:
+#    print("A két szám egyenlő.")
+#elif random_szam > megadott_szam:
+#    print("A megadott szám kisebb.")
+#elif random_szam < megadott_szam:
+#    print("A megadott szám nagyobb.")
+
+
 import random
+penzerme = ("fej", "írás")
+dobas = random.choice(penzerme)
 
-random_szam = random.randint(1, 3)
+kerdes = input("Fej vagy írás? ")
+print(f"A gép erre gondolt: {dobas}")
 
-megadott_szam = int(input("Adj meg egy számot! "))
-
-
-if random_szam == megadott_szam:
-    print("A két szám egyenlő.")
-elif random_szam > megadott_szam:
-    print("A megadott szám kisebb.")
-elif random_szam < megadott_szam:
-    print("A megadott szám nagyobb.")
+if kerdes == dobas:
+    print("Eltaláltad!")
+else:
+    print("Nem sikerült.")
---
+++ 06_fajl_ciklus.py
@@ -0,0 +1,23 @@
+# szam = 1
+# while szam <= 10:
+#     print(szam)
+# #    szam = szam + 1
+#     szam += 1
+
+# folytatja = True
+# while folytatja:
+#   print('Vidd ki a szemetet!')
+#   valasz = input('Mondjam még egyszer? (i/n)')
+#   if valasz == 'n':
+#       folytatja = False
+# print('>> Program vége! <<')
+
+
+# szam = 10
+# while szam <= 10:
+#     if szam % 2 == 0:
+#         print(szam)
+#     szam += 1
+
+szam = 10
+while
